87ac7964e069896be1c8eb05dbb56fc46c2346cb
webbcoder <ksk.coder@gmail.com>
2019-12-11 22:09:08 +0200

    - added thumbnail of post

--- news_site/urls.py
+++ news_site/urls.py
@@ -18,7 +18,6 @@ from django.urls import path
 from django.conf import settings
 from django.urls import include
 from .views import redirect_blog
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -28,5 +27,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    # urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
